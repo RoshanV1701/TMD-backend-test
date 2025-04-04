a9cf62f795a05e3324186a14afae2f3643e5f4de
Roshan Varghese <roshan.varghese@yahoo.in>
2025-04-04 15:02:40 -0700

    download dropbox data

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,18 @@
-module.exports = (req, res) => {
-    res.status(200).json({ message: 'Hello from Vercel backend!' });
-  };
-  
+const axios = require('axios');
+
+module.exports = async (req, res) => {
+  const dropboxUrl = 'https://www.dropbox.com/scl/fo/4f8nxi1mxbmjvohrxropd/ABytmHRqBtj7m9DikbsAG-0?e=1&preview=day_pattern.csv&rlkey=9krbwmbbmiln2p7qm0hmuo6l3&st=rvo9532j&dl=1';
+
+  try {
+    const response = await axios.get(dropboxUrl, {
+      responseType: 'arraybuffer', // Get raw binary data
+    });
+
+    res.setHeader('Content-Disposition', 'attachment; filename="data.csv"');
+    res.setHeader('Content-Type', 'text/csv');
+    res.status(200).send(response.data);
+  } catch (error) {
+    console.error('Error downloading file from Dropbox:', error.message);
+    res.status(500).json({ error: 'Failed to download file from Dropbox' });
+  }
+};
